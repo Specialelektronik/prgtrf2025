3a2e63542d9a7cc6f0dc66eed06250511316a3a7
Patrik Lindahl <patrik.lindahl@specialelektronik.se>
2025-08-25 16:34:29 +0200

    Lagt till Grundläggande C#-program

---
+++ Basic dotnet8 program/SE_Example_Program/ControlSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.InteropServices;
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;
+
+namespace SE_Example_Program;
+
+public class ControlSystem : CrestronControlSystem
+{
+    public ControlSystem() : base()
+    {
+        try
+        {
+            // Initialize Crestron threadpool
+            Crestron.SimplSharpPro.CrestronThread.Thread.MaxNumberOfUserThreads = 20;
+            
+            CrestronEnvironment.ProgramStatusEventHandler += new ProgramStatusEventHandler(ControllerProgramEventHandler);
+        }
+        catch (Exception e)
+        {
+            ErrorLog.Error("Error in the constructor: {0}", e.Message);
+        }
+    }
+    
+    public override void InitializeSystem()
+    {
+        try
+        {
+            ErrorLog.Notice($"Hello world!");
+        }
+        catch (Exception e)
+        {
+            ErrorLog.Error("Error in InitializeSystem: {0}", e.Message);
+        }
+    }
+    
+    void ControllerProgramEventHandler(eProgramStatusEventType programStatusEventType)  
+    {  
+        switch (programStatusEventType)  
+        {  
+            case (eProgramStatusEventType.Paused):  
+                // The program has been paused. Pause all user threads/timers as needed.  
+                break;  
+            case (eProgramStatusEventType.Resumed):  
+                // The program has been resumed. Resume all the user threads/timers as needed.
+                break;  
+            case (eProgramStatusEventType.Stopping):  
+                // The program has been stopped.  
+                // Close all threads.            
+                // Shutdown all Client/Servers in the system.  
+                // General cleanup. 
+                // Unsubscribe to all System Monitor events  
+                break;  
+        }  
+    }
+}
